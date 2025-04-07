51788a0810800d0d32c38a5877f164f7d24f90e4
JMichael-B <michael.b@lexcode.com>
2025-04-07 11:24:59 +0800

    Added AWS Server connection

--- src/socket.ts
+++ src/socket.ts
@@ -1,13 +1,19 @@
 import { io } from "socket.io-client";
 
 // FLASK SOCKETIO (LOCAL)
-export const socket = io("http://localhost:9000", {
-    path: "/socket.io/",
-    transports: ["websocket"],
-});
+// export const socket = io("http://localhost:9000", {
+//     path: "/socket.io/",
+//     transports: ["websocket"],
+// });
 
-// FLASK SOCKETIO (SERVER)
+// FLASK SOCKETIO (PH SERVER)
 // export const socket = io("wss://qurious.ddns.net", {
 //     path: "/qurious-engagement/socket.io/",
 //     transports: ["websocket"],
 // });
+
+// FLASK SOCKETIO (AWS SERVER)
+export const socket = io("https://api.qurious.lexcodeapi.com", {
+    path: "/engagement/socket.io/",
+    transports: ["websocket"],
+});
